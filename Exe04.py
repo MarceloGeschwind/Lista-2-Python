93e54248a5cd04bd5d1844464a9fa8251f0c8da7
MarceloGeschwind <marcelo.pimentel@ort.org.br>
2026-03-04 18:17:56 -0300

    Update Exe04.py

--- Exe04.py
+++ Exe04.py
@@ -1,6 +1,9 @@
 '''
 4. O Crachá do Funcionário (Multiplicação de Strings e Limpeza) 
-Faça um programa que leia o nome de um funcionário e seu cargo para gerar um crachá digital. Como os usuários às vezes digitam espaços sem querer no início ou fim, use o método .strip() para limpar a entrada. Para criar as bordas superior e inferior do crachá, utilize o operador de multiplicação de strings (ex: "-" * 30).
+Faça um programa que leia o nome de um funcionário e seu cargo para gerar um crachá digital. 
+Como os usuários às vezes digitam espaços sem querer no início ou fim, use o método .strip() 
+para limpar a entrada. Para criar as bordas superior e inferior do crachá, utilize o operador d
+e multiplicação de strings (ex: "-" * 30). 
 
 Exemplo de Entrada: 
 Nome: Harry 
@@ -13,11 +16,11 @@ Nome: Harry
 Cargo: Caixa 
 ------------------------------
 '''
-
-nome = ' Harry '
-cargo = 'caixa'
+# a função strip(), remove os espaços em branco do começo e do final da string
+nome = '      harry  '
+cargo = '    caixa '
 
 print('-' * 30)
-print('Nome:', nome.strip().capitalize)
-print('Cargo:', cargo.strip().capitalize)
+print('Nome:', nome.strip().capitalize())
+print('Cargo:', cargo.strip().capitalize())
 print('-' * 30)
