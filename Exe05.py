c5740f5bf54aba03511d6f94015548a6bfebc0a4
MarceloGeschwind <marcelo.pimentel@ort.org.br>
2026-03-04 18:17:33 -0300

    Update Exe05.py

--- Exe05.py
+++ Exe05.py
@@ -1,6 +1,11 @@
 '''
 5. Calculadora de Dano Crítico (Matemática e Formatação Mista) 
-Crie um programa que simule o cálculo de dano de um ataque em um jogo. Leia o nome do ataque, o valor do dano base e o multiplicador de acerto crítico (um número decimal). Calcule o dano total (dano base * multiplicador). Exiba na tela uma mensagem de combate emocionante, formatando o dano total com zero casas decimais (arredondamento visual no f-string) e destacando o nome do ataque em maiúsculas. 
+Crie um programa que simule o cálculo de dano de um ataque em um jogo. 
+Leia o nome do ataque, o valor do dano base e o multiplicador de acerto crítico 
+(um número decimal). Calcule o dano total (dano base * multiplicador). 
+Exiba na tela uma mensagem de combate emocionante, formatando o dano total 
+com zero casas decimais (arredondamento visual no f-string) e destacando o nome 
+do ataque em maiúsculas. 
 
 Exemplo de Entrada: 
 Ataque: corte ninja 
@@ -13,8 +18,8 @@ Dano causado: 225 pontos!
 '''
 
 ataque = 'corte ninja'
-dano = 15
+dano = 150
 multiplicador = 1.5
 
 print('O ataque {} foi um sucesso!'.format(ataque.upper()))
-print('Dano causado:{} pontos!'.format(int(dano*multiplicador)))
+print('Dano causado: {} pontos!'.format(int(dano * multiplicador)))
